bdfb2f1a335f0c771c89a2adce0c09c4cffc559a
NicolasFune <nicolas290@live.com>
2021-12-30 14:25:33 -0300

    30/12/2021

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let btn_add = document.querySelector(".btn-add")
+let text_task = document.querySelector("#text-task").value
+
+
+btn_add.addEventListener("click",AdicionarTask())
+function AdicionarTask(task){
+    alert("teste")
+}
