f9ba1bfae25eecbe096a31aabdbe13607a5874f0
NicolasFune <nicolas290@live.com>
2022-01-28 12:08:42 -0300

    28/01/2022

--- api/index.php
+++ api/index.php
@@ -20,10 +20,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="shortcut icon" href="img/tasks.png" type="image/x-icon">
-    <link rel="stylesheet" href="bootstrap-5/css/bootstrap.css">
+    <link rel="shortcut icon" href="../img/tasks.png" type="image/x-icon">
+    <link rel="stylesheet" href="../bootstrap-5/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Tasks App - Login</title>
 </head>
 <body class="d-flex text-white justify-content-between flex-column  " style="background-image: linear-gradient( #65a8f5 , #2d80e2  ); background-repeat: no-repeat; height: 100vh;">
@@ -32,7 +32,7 @@
 
         <h1 class="text-white text-center mb-5" style="font-family: 'login-medium';">Sing In</h1>
 
-        <form class="text-white" action="http://localhost/Tasks_App/php/login.php" method="post">
+        <form class="text-white" action="http://localhost/Tasks_App/api/php/login.php" method="post">
             <div class="form-group ">
                 <label for="email-value" style="font-family: 'login-bold'; font-size: .8rem;">E-mail</label>
                 <div class="input-group shadow ">
@@ -65,7 +65,7 @@
         <a class="nav-link text-white d-inline-block h-25 py-0  " href="php/register.php" style="font-family: 'login-bold';font-size: .9rem; line-height: 1rem;">Registre-se</a>
     </div>
     
-    <script src="bootstrap-5/js/bootstrap.js"></script>
-    <script src="js/index.js"></script>
+    <script src="../bootstrap-5/js/bootstrap.js"></script>
+    <script src="../js/index.js"></script>
 </body>
 </html>
